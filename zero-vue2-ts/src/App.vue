eba24aa3913ab26a533e5f2749e575b69e89dcb8
Micro-Organism <coolrabbit1993@gmail.com>
2024-08-27 16:26:54 +0800

    feat: 添加zero-vue2-ts版本

---
+++ zero-vue2-ts/src/App.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: Micro-Organism
+ * @Date: 2024/8/27 15:22
+ * @FilePath: zero-vue2-ts\src\App.vue
+ * @Email: coolrabbit1993@gmail.com
+ * @Description: App 程序入口
+-->
+<template>
+  <div id="app">
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+<!--    <HelloWorld msg="Vite + Vue" />-->
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
